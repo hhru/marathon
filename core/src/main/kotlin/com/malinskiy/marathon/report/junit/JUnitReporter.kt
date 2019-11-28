e7667d73bff074f28e96142b1ff3ef9ff22e4ca4
Pavel Strelchenko <p.strelchenko@hh.ru>
2019-11-28 14:05:38 +0300

    Generating succeeded JUnit XML result file if there were at least one success event for current test

--- core/src/main/kotlin/com/malinskiy/marathon/report/junit/JUnitReporter.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/report/junit/JUnitReporter.kt
@@ -1,12 +1,36 @@
 package com.malinskiy.marathon.report.junit
 
 import com.malinskiy.marathon.analytics.internal.sub.ExecutionReport
+import com.malinskiy.marathon.analytics.internal.sub.TestEvent
 import com.malinskiy.marathon.report.Reporter
+import com.malinskiy.marathon.test.Test
 
 internal class JUnitReporter(private val jUnitWriter: JUnitWriter) : Reporter {
     override fun generate(executionReport: ExecutionReport) {
-        executionReport.testEvents.forEach { event ->
+
+        val testEvents = collectTestEvents(executionReport)
+        testEvents.forEach { event ->
             jUnitWriter.testFinished(event.poolId, event.device, event.testResult)
+        }
+    }
+
+    private fun collectTestEvents(executionReport: ExecutionReport): List<TestEvent> {
+        val testEventsMap = mutableMapOf<Test, MutableList<TestEvent>>()
+
+        executionReport.testEvents.forEach { event ->
+            val storedEvents = testEventsMap[event.testResult.test] ?: mutableListOf()
+            storedEvents += event
+
+            testEventsMap[event.testResult.test] = storedEvents
+        }
+
+        return testEventsMap.flatMap { entry ->
+            val successItem = entry.value.find { it.testResult.isSuccess }
+            if (successItem == null) {
+                entry.value
+            } else {
+                listOf(successItem)
+            }
         }
     }
 }
